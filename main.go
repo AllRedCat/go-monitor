0cf629e63090701f616d341efb11e4602a6e15f4
AllRedCat <leonismogabriel@gmail.com>
2025-09-01 19:53:03 -0300

    📌 Feat: add routee to get CPU info - The route gets info of each logical CPU

--- main.go
+++ main.go
@@ -16,15 +16,28 @@ import (
 
 // Struct to hold system metrics
 type Metrics struct {
-	CPUPercent float64 `json:"cpu_percent"`
-	Memory     uint64  `json:"memory_total"`
-	MemoryUsed uint64  `json:"memory_used"`
-	MemoryFree uint64  `json:"memory_free"`
-	DiskTotal  uint64  `json:"disk_total"`
-	DiskUsed   uint64  `json:"disk_used"`
-	DiskFree   uint64  `json:"disk_free"`
-	NetSent    uint64  `json:"net_sent"`
-	NetRecv    uint64  `json:"net_recv"`
+	CPUPercent  float64 `json:"cpu_percent"`
+	Memory      uint64  `json:"memory_total"`
+	MemoryUsed  uint64  `json:"memory_used"`
+	MemoryFree  uint64  `json:"memory_free"`
+	DiskTotal   uint64  `json:"disk_total"`
+	DiskUsed    uint64  `json:"disk_used"`
+	DiskFree    uint64  `json:"disk_free"`
+	DiskPercent float64 `json:"disk_percent"`
+	NetSent     uint64  `json:"net_sent"`
+	NetRecv     uint64  `json:"net_recv"`
+}
+
+type CPUInfo struct {
+	CPUI        int32   `json:"cpu"`
+	SteppingI   int32   `json:"stepping"`
+	PhysicalIdI string  `json:"physicalId`
+	CoreIdI     string  `json:"coreId"`
+	CoresI      int32   `json:"cores"`
+	ModelNameI  string  `json:"modelName"`
+	MhzI        float64 `json:"mhz"`
+	ChaceSizeI  int32   `json:"cacheSize"`
+	MicrocodeI  string  `json:"microencode"`
 }
 
 // WebSocket upgrader
@@ -35,6 +48,25 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
+	// Route to get machine info
+	http.HandleFunc("/info", func(w http.ResponseWriter, r *http.Request) {
+		info, err := getMachineInfo() // Call function to get system info
+		// Check if a error appears
+		if err != nil {
+			log.Println("Error getting info:", err)
+			json.NewEncoder(w).Encode(err)
+		}
+
+		// Send info in JSON format
+		json.NewEncoder(w).Encode(info)
+	})
+
+	// Route to test get CPU usage info
+	http.HandleFunc("/cpu", func(w http.ResponseWriter, r *http.Request) {
+		
+	})
+
+	// WebSocket to get metrics with a time of 3 seconds
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		conn, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
@@ -44,11 +76,13 @@ func main() {
 		defer conn.Close()
 
 		for {
+			log.Println("Request from:", r.RemoteAddr)
 			metrics, err := getMetrics()
 			if err != nil {
 				log.Println("Error getting metrics:", err)
 				break
 			}
+			log.Println("Metrics:", metrics)
 
 			data, _ := json.Marshal(metrics)
 			err = conn.WriteMessage(websocket.TextMessage, data)
@@ -65,6 +99,30 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe error:", err)
 	}
+}
+
+func getMachineInfo() ([]CPUInfo, error) {
+	cpus, err := cpu.Info()
+	if err != nil {
+		return nil, err
+	}
+
+	var infos []CPUInfo
+	for _, i := range cpus {
+		infos = append(infos, CPUInfo{
+			CPUI:        i.CPU,
+			SteppingI:   i.Stepping,
+			PhysicalIdI: i.PhysicalID,
+			CoreIdI:     i.CoreID,
+			CoresI:      i.Cores,
+			ModelNameI:  i.ModelName,
+			MhzI:        i.Mhz,
+			ChaceSizeI:  i.CacheSize,
+			MicrocodeI:  i.Microcode,
+		})
+	}
+
+	return infos, nil
 }
 
 func getMetrics() (Metrics, error) {
@@ -92,15 +150,16 @@ func getMetrics() (Metrics, error) {
 	netIO := netIOs[0]
 
 	metrics := Metrics{
-		CPUPercent: cpuPercent,
-		Memory:     v.Total,
-		MemoryUsed: v.Used,
-		MemoryFree: v.Free,
-		DiskTotal:  d.Total,
-		DiskUsed:   d.Used,
-		DiskFree:   d.Free,
-		NetSent:    netIO.BytesSent,
-		NetRecv:    netIO.BytesRecv,
+		CPUPercent:  cpuPercent,
+		Memory:      v.Total,
+		MemoryUsed:  v.Used,
+		MemoryFree:  v.Free,
+		DiskTotal:   d.Total,
+		DiskUsed:    d.Used,
+		DiskFree:    d.Free,
+		DiskPercent: d.UsedPercent,
+		NetSent:     netIO.BytesSent,
+		NetRecv:     netIO.BytesRecv,
 	}
 
 	return metrics, nil
